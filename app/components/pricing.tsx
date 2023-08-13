9ad7aea92678390deb5acf8e54c8dfdeda79c968
Connor <connor.paul.stauffer@gmail.com>
2023-08-13 14:22:09 -0600

    Begin styling pricing

--- app/components/pricing.tsx
+++ app/components/pricing.tsx
@@ -18,9 +18,9 @@ export function Pricing() {
             in. Explicabo id ut laborum.
           </p>
         </div>
-        <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-white/10 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+        <div className="mx-auto mt-16 max-w-2xl rounded-3xl bg-white/5 ring-1 ring-indigo-500 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
           <div className="p-8 sm:p-10 lg:flex-auto">
-            <h3 className="text-2xl font-bold tracking-tight text-white">Lifetime membership</h3>
+            <h3 className="text-2xl font-bold tracking-tight text-white">Weekly subscription</h3>
             <p className="mt-6 text-base leading-7 text-gray-300">
               Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
               repellendus etur quidem assumenda.
@@ -42,18 +42,18 @@ export function Pricing() {
             </ul>
           </div>
           <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-            <div className="rounded-2xl bg-white/5 py-10 text-center ring-1 ring-inset ring-indigo-500 lg:flex lg:flex-col lg:justify-center lg:py-16">
+            <div className="rounded-2xl bg-gray-900 py-10 text-center ring-1 ring-inset ring-white/10 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
-                <p className="text-base font-semibold text-gray-300">Pay once, own it forever</p>
+                <p className="text-base font-semibold text-gray-300">Cancel anytime</p>
                 <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                  <span className="text-5xl font-bold tracking-tight text-white">$349</span>
+                  <span className="text-5xl font-bold tracking-tight text-white">$2,500</span>
                   <span className="text-sm font-semibold leading-6 tracking-wide text-gray-300">USD</span>
                 </p>
                 <a
                   href="#"
                   className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Get access
+                  Work with me
                 </a>
                 <p className="mt-6 text-xs leading-5 text-gray-300">
                   Invoices and receipts available for easy company reimbursement
