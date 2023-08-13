beab806f074d0b0cde140ea3927929ee7376ebff
Connor <connor.paul.stauffer@gmail.com>
2023-08-13 14:43:17 -0600

    Add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import { Hero } from './components/hero'
+// import { HeroSimple } from './components/hero-simple'
+// import { Features } from './components/features'
+import { FeaturesSimple } from './components/features-simple'
 import { Pricing } from './components/pricing'
+import { Faq } from './components/faq'
+import { Footer } from './components/footer'
 
 export default function Example() {
   return (
     <>
       <Hero />
+      {/* <HeroSimple /> */}
+      {/* <Features /> */}
+      <FeaturesSimple />
       <Pricing />
+      <Faq />
+      <Footer />
     </>
   )
 }
